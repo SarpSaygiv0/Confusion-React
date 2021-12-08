9797f7c098ba61d52a481f7b0bd35c27b47a52ef
Sarp Saygi <sarpalisaygi@gmail.com>
2021-12-08 04:01:26 +0200

    redux actions

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,6 +3,12 @@ import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap'
 import { Link } from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val); 
+
  class Contact extends Component {
     
     constructor(props) {
@@ -67,8 +73,21 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
                                 <Col md={10}>  {/* equals to <div className="col-md-10"></div> */}  
                                     <Control.text model=".firstName" id="firstName" name="firstName"
                                     placeholder="First Name" 
-                                    className="form-control"> 
-                                    </Control.text>
+                                    className="form-control"
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                    }}> 
+                                    </Control.text> 
+                                    <Errors 
+                                    className="text-danger"
+                                    model=".firstName"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 3',
+                                        maxLength: 'Must be 15 char or less'
+                                    }}
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -76,9 +95,21 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
                                 <Col md={10}>   
                                     <Control.text model=".lastName" id="lastName" name="lastName"
                                     placeholder="Last Name" 
-                                    className="form-control" 
-                                    > 
+                                    className="form-control"
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                    }}> 
                                     </Control.text>
+                                    <Errors 
+                                    className="text-danger"
+                                    model=".lastName"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 3',
+                                        maxLength: 'Must be 15 char or less'
+                                    }} 
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -86,16 +117,41 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
                                 <Col md={10}>   
                                     <Control.text model=".telnum" id="telnum" name="telnum"
                                     placeholder="Tel. Number"
-                                    className="form-control"> 
+                                    className="form-control"
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15), isNumber
+                                    }}> 
                                     </Control.text>
+                                    <Errors 
+                                    className="text-danger"
+                                    model=".telnum"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 3',
+                                        maxLength: 'Must be 15 numbers or less',
+                                        isNumber: 'Must be a number'
+                                    }} 
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
                                 <Label htmlFor="email" md={2}>Email</Label> 
                                 <Col md={10}>   
                                     <Control.text model=".email" id="email" name="email"
-                                    placeholder="Email" className="form-control"> 
+                                    placeholder="Email" className="form-control"
+                                    validators={{
+                                        required, validEmail
+                                    }}>  
                                     </Control.text>
+                                    <Errors 
+                                    className="text-danger"
+                                    model=".email"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        validEmail: 'Invalid email address'
+                                    }} />
                                 </Col>
                             </Row>
                             <Row className="form-group">
--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,7 +1,83 @@
-import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, Label, ModalHeader, ModalBody, Row } from 'reactstrap';
 import {Link} from "react-router-dom";
+import {Control, LocalForm, Errors} from 'react-redux-form';
 
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component {
+    constructor(props){
+        super(props);
+
+        this.toggleModal = this.toggleModal.bind(this);    
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            isModalOpen: false
+        }
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
+
+    render(){
+        return(
+            <div>
+            <Button type="button" className="btn btn-outline-secondary" outline onClick={this.toggleModal}>
+                Submit Comment
+            </Button>
+
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Label htmlFor="rating">Rating</Label>
+                        <Control.select model=".rating" id="rating" name="rating" 
+                        className="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Control.select> 
+                        <Label htmlFor="author">Your Name</Label>
+                        <Control.text model=".author" id="author" name="author" 
+                        className="form-control" placeholder="Your Name"
+                        validators={{minLength: minLength(3), maxLength: maxLength(15)}}>
+                        </Control.text> 
+                        <Errors 
+                            className="text-danger"
+                            model=".author"
+                            show="touched"
+                            messages={{
+                                minLength: 'Must be greater than 3',
+                                maxLength: 'Must be 15 char or less'
+                            }}
+                            />
+                        <Label htmlFor="comment">Comment</Label>
+                        <Control.textarea model=".comment" id="comment" name="comment" 
+                        className="form-control" rows="6">        
+                        </Control.textarea> 
+                        <Row className="form-group" />
+                        <Button type="submit" color="primary">
+                            Submit 
+                        </Button>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+            </div>
+            );
+    }
+}
     function RenderDish({dish}) {
         if(dish!=null) {
             return (
@@ -12,6 +88,7 @@ import {Link} from "react-router-dom";
                     <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
+
             );
         }
         else{
@@ -19,7 +96,7 @@ import {Link} from "react-router-dom";
         }
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if (comments.length != 0) {
                 return(
                     <div>
@@ -32,6 +109,7 @@ import {Link} from "react-router-dom";
                                 </li>
                             </ul>
                         ))}
+                        <CommentForm dishId={dishId} addComment={addComment} />
                     </div>
                 )
             };
@@ -57,7 +135,8 @@ import {Link} from "react-router-dom";
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                         addComment={props.addComment} dishId={props.dish.id} />
                     </div>
                 </div>
                 </div>
